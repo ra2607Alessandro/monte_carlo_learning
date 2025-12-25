64e94455e3dc985077acdd71ac916858cd1814fb
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-12-25 08:31:08 +0100

    BM with mean and volatility

--- bm.py
+++ bm.py
@@ -12,7 +12,9 @@ steps = 1000
 
 
 #set mean (in standard BM is 0) and volatily which is unity
-mean, sigma = 0.0 , 1.0
+#mean, sigma = 0.0 , 1.0
+# to include mean and volatility:
+mean, sigma= 5.0, 2.0
 
 # use rng.normal and create a matrix 2x2 with the mean, sigma, paths and times steps as factors
 Z = rng.normal(mean, sigma, (paths, steps))
@@ -21,26 +23,26 @@ Z = rng.normal(mean, sigma, (paths, steps))
 interval = [ 0.0 ,1.0 ]
 dt = ( interval[1] - interval[0] ) / ( steps - 1 ) 
 
-timeline = np.linspace(interval[0] , interval[1] , steps)
+timeline = np.linspace(interval[0], interval[1], steps)
 
 # create a new series with zeros with numpy starting from the paths and points and create a loop for the standard BM:
 W = np.zeros((paths,steps))
 # use [:, idx] which is the vectorized notation 
 for idx in range(steps - 1):
-    W[ : , idx + 1 ] = W[:,idx] + np.sqrt(dt) * Z[:,idx]
+    W[ : , idx + 1 ] = W[:,idx] + mean*dt + sigma*np.sqrt(dt) * Z[:,idx]
 
 fig, ax = plt.subplots(1, 1, figsize=(12, 8)) #use the subplots componenet
 #loop around the paths, plot the time plot and the series of 0 with this vectorized path W[path,:], then show
 for path in range(paths):
     ax.plot(timeline,W[path,:])
 
-#ax.set_xlabel("Time")
-#ax.set_ylabel("Value")
+ax.set_xlabel("Time")
+ax.set_ylabel("Value")
 
 
-final_values = pd.DataFrame({'final_values': W[:, -1]}) # creating a Dataframe with pandas
-sns.kdeplot(data=final_values, x='final_values', fill=True, ax=ax) # using seaborn for a kernel that estimates prob distr from a finite sample data
-ax.set_ylim(0.0, 0.325)
-ax.set_xlabel('Final Values of Asset Paths')
+#final_values = pd.DataFrame({'final_values': W[:, -1]}) # creating a Dataframe with pandas
+#sns.kdeplot(data=final_values, x='final_values', fill=True, ax=ax) # using seaborn for a kernel that estimates prob distr from a finite sample data
+#ax.set_ylim(0.0, 0.325)
+#ax.set_xlabel('Final Values of Asset Paths')
 plt.show()
 
