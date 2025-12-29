bcb6e7ac716b91bc7608d08c55b1130cfc18b8c1
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-12-29 08:54:41 +0100

    let's see with the given code

--- gbm.py
+++ gbm.py
@@ -1,3 +1,5 @@
+# gbm.py
+
 import os
 import random
 import string
@@ -6,8 +8,44 @@ import click
 import numpy as np
 import pandas as pd
 
-class GMB:
-    
+
+class GeometricBrownianMotionAssetSimulator:
+    """
+    This callable class will generate a daily
+    open-high-low-close-volume (OHLCV) based DataFrame to simulate
+    asset pricing paths with Geometric Brownian Motion for pricing
+    and a Pareto distribution for volume.
+
+    It will output the results to a CSV with a randomly generated
+    ticker smbol.
+
+    For now the tool is hardcoded to generate business day daily
+    data between two dates, inclusive.
+
+    Note that the pricing and volume data is completely uncorrelated,
+    which is not likely to be the case in real asset paths.
+
+    Parameters
+    ----------
+    start_date : `str`
+        The starting date in YYYY-MM-DD format.
+    end_date : `str`
+        The ending date in YYYY-MM-DD format.
+    output_dir : `str`
+        The full path to the output directory for the CSV.
+    symbol_length : `int`
+        The length of the ticker symbol to use.
+    init_price : `float`
+        The initial price of the asset.
+    mu : `float`
+        The mean 'drift' of the asset.
+    sigma : `float`
+        The 'volatility' of the asset.
+    pareto_shape : `float`
+        The parameter used to govern the Pareto distribution
+        shape for the generation of volume data.
+    """
+
     def __init__(
         self,
         start_date,
@@ -17,7 +55,8 @@ class GMB:
         init_price,
         mu,
         sigma,
-        pareto_shape):
+        pareto_shape
+    ):
         self.start_date = start_date
         self.end_date = end_date
         self.output_dir = output_dir
@@ -26,73 +65,178 @@ class GMB:
         self.mu = mu
         self.sigma = sigma
         self.pareto_shape = pareto_shape
-    
-    def generate_random_symbol(self):
-        return " ".join(random.choice(string.ascii_uppercase,k = self.symbol_length))
-    
-    def create_empty_frame(self):
+
+    def _generate_random_symbol(self):
+        """
+        Generates a random ticker symbol string composed of
+        uppercase ASCII characters to use in the CSV output filename.
+
+        Returns
+        -------
+        `str`
+            The random ticker string composed of uppercase letters.
+        """
+        return ''.join(
+            random.choices(
+                string.ascii_uppercase,
+                k=self.symbol_length
+            )
+        )
+
+    def _create_empty_frame(self):
+        """
+        Creates the empty Pandas DataFrame with a date column using
+        business days between two dates. Each of the price/volume
+        columns are set to zero.
+
+        Returns
+        -------
+        `pd.DataFrame`
+            The empty OHLCV DataFrame for subsequent population.
+        """
         date_range = pd.date_range(
-        self.start_date,
-        self.end_date,
-        freq='B')
-        zeros = pd.Series(np.zeros(len(date_range)))#create a zero for every single data
+            self.start_date,
+            self.end_date,
+            freq='B'
+        )
+
+        zeros = pd.Series(np.zeros(len(date_range)))
+
         return pd.DataFrame(
             {
                 'date': date_range,
-                'open':zeros,
+                'open': zeros,
                 'high': zeros,
                 'low': zeros,
+                'close': zeros,
                 'volume': zeros
-
             }
-        )[['date','open','high','low','volume']]
-        
+        )[['date', 'open', 'high', 'low', 'close', 'volume']]
 
-    def GMB_simulation(self, data):
+    def _create_geometric_brownian_motion(self, data):
+        """
+        Calculates an asset price path using the analytical solution
+        to the Geometric Brownian Motion stochastic differential
+        equation (SDE).
+
+        This divides the usual timestep by four so that the pricing
+        series is four times as long, to account for the need to have
+        an open, high, low and close price for each day. These prices
+        are subsequently correctly bounded in a further method.
+
+        Parameters
+        ----------
+        data : `pd.DataFrame`
+            The DataFrame needed to calculate length of the time series.
+
+        Returns
+        -------
+        `np.ndarray`
+            The asset price path (four times as long to include OHLC).
+        """
         n = len(data)
-        T = 242 #business days in a year
-        dt = T/(4*n) # we need 4 prices x day
+        T = n / 252.0  # Business days in a year
+        dt = T / (4.0 * n)  # 4.0 is needed as four prices per day are required
+        
+        # Vectorised implementation of asset path generation
+        # including four prices per day, used to create OHLC
+        asset_path = np.exp(
+            (self.mu - self.sigma**2 / 2) * dt +
+            self.sigma * np.random.normal(0, np.sqrt(dt), size=(4 * n))
+        )
+        
+        return self.init_price * asset_path.cumprod()
 
-        asset_price = np.exp((self.mu - (self.sigma**2)/2)*dt + self.sigma*random.normal(0,np.sqrt(dt),size = 4 * n))
-        return self.init_price * asset_price.cumprod() 
-    
-    def append_data_to_csv(self,data,path):
+    def _append_path_to_data(self, data, path):
+        """
+        Correctly accounts for the max/min calculations required
+        to generate a correct high and low price for a particular
+        day's pricing.
+
+        The open price takes every fourth value, while the close
+        price takes every fourth value offset by 3 (last value in
+        every block of four).
+
+        The high and low prices are calculated by taking the max
+        (resp. min) of all four prices within a day and then
+        adjusting these values as necessary.
+
+        This is all carried out in place so the frame is not returned
+        via the method.
+
+        Parameters
+        ----------
+        data : `pd.DataFrame`
+            The price/volume DataFrame to modify in place.
+        path : `np.ndarray`
+            The original NumPy array of the asset price path.
+        """
         data['open'] = path[0::4]
         data['close'] = path[3::4]
 
         data['high'] = np.maximum(
-                 np.maximum(path[0::4],path[1::4]),
-                 np.maximum(path[2::4],path[3::4])
+            np.maximum(path[0::4], path[1::4]),
+            np.maximum(path[2::4], path[3::4])
         )
+
         data['low'] = np.minimum(
-            np.minimum(path[0::4],path[1::4]),
-            np.minimum(path[2::4],path[3::4])
+            np.minimum(path[0::4], path[1::4]),
+            np.minimum(path[2::4], path[3::4])
         )
-    
-    def append_volume_to_data(self,data):
-        data = np.array(
-            map(
-                int,
-                np.random.pareto(
-                    self.pareto_shape,
-                    size=len(data)
-                )*1000000
+
+    def _append_volume_to_data(self, data):
+        """
+        Utilises a Pareto distribution to simulate non-negative
+        volume data. Note that this is not correlated to the
+        underlying asset price, as would likely be the case for
+        real data, but it is a reasonably effective approximation.
+
+        Parameters
+        ----------
+        data : `pd.DataFrame`
+            The DataFrame to append volume data to, in place.
+        """
+        data['volume'] = np.array(
+            list(
+                map(
+                    int,
+                    np.random.pareto(
+                        self.pareto_shape,
+                        size=len(data)
+                    ) * 1000000.0
+                )
             )
         )
-    
-    def output_frame_to_dir(self,symbol,data):
+
+    def _output_frame_to_dir(self, symbol, data):
+        """
+        Output the fully-populated DataFrame to disk into the
+        desired output directory, ensuring to trim all pricing
+        values to two decimal places.
+
+        Parameters
+        ----------
+        symbol : `str`
+            The ticker symbol to name the file with.
+        data : `pd.DataFrame`
+            The DataFrame containing the generated OHLCV data.
+        """
         output_file = os.path.join(self.output_dir, '%s.csv' % symbol)
         data.to_csv(output_file, index=False, float_format='%.2f')
 
-
     def __call__(self):
-        symbol = self.generate_random_symbol()
-        frame = self.create_empty_frame()
-        GMB = self.GMB_simulation(frame)
-        csv = self.append_data_to_csv(frame, GMB)
-        volume = self.append_volume_to_data(frame)
-        output = self.output_frame_to_dir(symbol,frame)
-
+        """
+        The entrypoint for generating the asset OHLCV frame. Firstly this
+        generates a symbol and an empty frame. It then populates this
+        frame with some simulated GBM data. The asset volume is then appended
+        to this data and finally it is saved to disk as a CSV.
+        """
+        symbol = self._generate_random_symbol()
+        data = self._create_empty_frame()
+        path = self._create_geometric_brownian_motion(data)
+        self._append_path_to_data(data, path)
+        self._append_volume_to_data(data)
+        self._output_frame_to_dir(symbol, data)
 
 
 @click.command()
@@ -119,7 +263,7 @@ def cli(num_assets, random_seed, start_date, end_date, output_dir, symbol_length
     random.seed(random_seed)
     np.random.seed(seed=random_seed)
 
-    gbmas = GMB(
+    gbmas = GeometricBrownianMotionAssetSimulator(
         start_date,
         end_date,
         output_dir,
@@ -136,7 +280,6 @@ def cli(num_assets, random_seed, start_date, end_date, output_dir, symbol_length
         print('Generating asset path %d of %d...' % (i+1, num_assets))
         gbmas()
 
-        
 
 if __name__ == "__main__":
-    click()
+    cli()
