0899b55c7a9cb6da601d1210c619505206a8e58f
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-12-30 12:02:57 +0100

    brosky

--- greeks.py
+++ greeks.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 # The Math: Finite Difference Greeks
 #Since Monte Carlo gives us a "price," we calculate Greeks by bumping the input slightly and seeing how the price changes.
 #Delta :
@@ -8,3 +10,8 @@
  
 #Vega: 
 # formula = (V*(sigma + h) - V*(sigma - h))/(2*h)
+
+def montecarlo_pricing(S,T,K,sigma,r  ):
+    ST = S*np.exp((r - (0.5*((sigma**2)/2)))*T + sigma*(np.sqrt(T)*np.random.normal()))
+    payoff = np.maximum(ST - K, 0)
+    return np.exp(-1*(r*T)) * np.mean(payoff)
