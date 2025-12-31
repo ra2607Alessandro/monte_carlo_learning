1fd436488e3e9d1822637aa8040f5d8741565816
ra2607Alessandro <2alessandro.aimar@gmail.com>
2025-12-31 12:23:01 +0100

    yes

--- greeks.py
+++ greeks.py
@@ -39,7 +39,7 @@ def calculate_greeks(S,K,T,sigma,r,iterations ):
     d_sigma_minus = montecarlo_pricing(S,T,K,sigma - vol_h,r,Z)
     vega = (d_sigma_plus - d_sigma_minus)/(2*vol_h)
 
-    #return the greeks
+    #return the greeks as a dictionary (object)
     return {
         "Price": price_mid,
         "Delta": delta,
@@ -49,5 +49,7 @@ def calculate_greeks(S,K,T,sigma,r,iterations ):
 
 results = calculate_greeks(S=100, K=100, r=0.05, sigma=0.2, T=1, iterations= 1000000  )
 
-for greek, value in results.items():
-    print(f"{greek}: {value:.4f}")
+for greek, value in results.items(): #since the greeks come as dictionaries (objects), items paires the name with the value
+    print(f"{greek}: {value:.2f}") # .4f just says to reduce the number to 4 decimals after the point
+
+    # vega is supposed to be bigger because if you bump up sigma it results in a much bigger bump than if you bump up S a bit
