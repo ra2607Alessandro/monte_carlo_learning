86ef8962de42470215d93f54b0078e9f010bea49
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-02 11:46:15 +0100

    maybe i just don't understand shit

--- vanilla_option_class/vanilla_option.py
+++ vanilla_option_class/vanilla_option.py
@@ -7,5 +7,15 @@ class Vanilla:
         self.T = T
         self.option_type = option_type
 
-    
+    def payoff(self,S,r,sigma,iters):
+        Z = np.random.normal(size=iters)
+        ST = S*np.exp((r-0.5*(sigma**2)) + sigma*np.sqrt(self.T)*Z)
+        payoff = 0
+        if self.option_type == "call":
+            payoff = np.maximum(ST-self.K,0)
+        elif self.option_type == "put":
+            payoff = np.minimum(ST-self.K, 0)
+        
+        return payoff
+
         
