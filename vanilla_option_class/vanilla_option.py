d14eb5642a2db86ed52feb75a25a24e527f139f5
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-02 12:05:06 +0100

    i'd be screwed

--- vanilla_option_class/vanilla_option.py
+++ vanilla_option_class/vanilla_option.py
@@ -7,15 +7,24 @@ class Vanilla:
         self.T = T
         self.option_type = option_type
 
-    def payoff(self,S,r,sigma,iters):
-        Z = np.random.normal(size=iters)
-        ST = S*np.exp((r-0.5*(sigma**2)) + sigma*np.sqrt(self.T)*Z)
+    def payoff(self,ST):
+        
         payoff = 0
         if self.option_type == "call":
             payoff = np.maximum(ST-self.K,0)
         elif self.option_type == "put":
             payoff = np.maximum(self.K - ST, 0)
+        elif len(ST)> 1:
+            ST_terminal = ST[:,-1]
+            arr = np.array(ST_terminal)
+            return arr
         
         return payoff
-
-        
+    
+    def simulate_terminal(self,S,r,sigma,iters):
+        Z = np.random.normal(size=iters)
+        ST = S*np.exp((r-0.5*(sigma**2))*self.T + sigma*np.sqrt(self.T)*Z)
+        return ST
+    
+    def discounted_payoff(self,r,ST):
+        return np.exp(-r*self.T)*self.payoff(ST)
