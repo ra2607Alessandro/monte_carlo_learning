d5bfc08d7143ce8e326ee21ffeb238a75dfb9bc0
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-06 20:05:06 +0100

    bur

--- classes/engine.py
+++ classes/engine.py
@@ -35,10 +35,10 @@ class MonteCarloEngine:
         h = S * 0.01
         vol_h = 0.01
         price_plus = vanilla.discounted_payoff(ST=vanilla.simulate_terminal(S=S + h,r=r,sigma=sigma, Z=Z),r=r)
-        price = Vanilla.discounted_payoff(ST=Vanilla.simulate_terminal(S=S ,r=r,sigma=sigma, Z=Z),r=r)
-        price_minus = Vanilla.discounted_payoff(ST=Vanilla.simulate_terminal(S=S - h,r=r,sigma=sigma, Z=Z),r=r)
-        vol_plus = Vanilla.discounted_payoff(ST=Vanilla.simulate_terminal(S=S ,r=r,sigma=sigma + vol_h, Z=Z),r=r)
-        vol_minus = Vanilla.discounted_payoff(ST=Vanilla.simulate_terminal(S=S,r=r,sigma=sigma - vol_h, Z=Z),r=r)
+        price = vanilla.discounted_payoff(ST=vanilla.simulate_terminal(S=S ,r=r,sigma=sigma, Z=Z),r=r)
+        price_minus = vanilla.discounted_payoff(ST=vanilla.simulate_terminal(S=S - h,r=r,sigma=sigma, Z=Z),r=r)
+        vol_plus = vanilla.discounted_payoff(ST=vanilla.simulate_terminal(S=S ,r=r,sigma=sigma + vol_h, Z=Z),r=r)
+        vol_minus = vanilla.discounted_payoff(ST=vanilla.simulate_terminal(S=S,r=r,sigma=sigma - vol_h, Z=Z),r=r)
         return {
             'price bump' : h,
             'sigma bump' : vol_h,
