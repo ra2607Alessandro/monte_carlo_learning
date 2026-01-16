461ed63a552e772b626eac6a3bd4e6ce84b4c891
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-16 17:24:25 +0100

    check

--- classes/engine.py
+++ classes/engine.py
@@ -132,16 +132,19 @@ class MonteCarloEngine:
             raise ValueError('greek can only be delta, gamma, vega, rho')
         
     def implied_volatility(self,sigma,market_price,S,K,T,r):
+        iterations = 0
         BS = self.price(S=S,r=r,sigma=sigma,K=K,T=T,n_sims=10000,method='antithetic')
         BS_price = BS['price']
-        while BS_price - market_price < 0.01:
+        while BS_price - market_price < 0.01 and iterations < 51:
 
           vega = self.greeks(greek='vega',sigma=sigma,K=K,S=S,r=r,T=T)
           sigma_new = sigma - (BS_price - market_price)/vega 
           if BS_price > market_price:
             sigma = sigma - 0.01
+            iterations = iterations + 1
           elif market_price > BS_price:
             sigma = sigma + 0.01
+            iterations = iterations + 1
           
 
         return sigma_new
