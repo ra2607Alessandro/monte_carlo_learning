4391dcb68aa6504e5f34821b4c0897f9f55045f7
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-25 09:25:28 +0100

    a bit rusty and stupid

--- eur_usd.py
+++ eur_usd.py
@@ -1,7 +1,21 @@
 #calculate volatility
 import pandas as pd
+import numpy as np
 df = pd.read_csv('EUR_USD.csv').set_index('Date')
-def volatility():
+df = df.sort_index() # from old to new 
+
+price = float(df['Close'])
+log_ret = np.log(price/price.shift(1)).dropna()
+df['log_ret'] = log_ret
+
+window = [10, 20, 60]
+ 
+for i in range(len(window)):
+
+    standard_deviation = df['log_ret'].rolling(i).std()
+    print(f'Standard deviation in {i}-days:{standard_deviation}') 
+
+   
     
 
 #calculate 50-day MA
