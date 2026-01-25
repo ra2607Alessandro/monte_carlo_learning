dbe837aeb5afc7baf7fa937ec46d30231111d7ac
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-25 09:42:36 +0100

    yes bro

--- eur_usd.py
+++ eur_usd.py
@@ -4,7 +4,7 @@ import numpy as np
 df = pd.read_csv('EUR_USD.csv').set_index('Date')
 df = df.sort_index() # from old to new 
 
-price = float(df['Close'])
+price = df['Close'].astype(float)
 log_ret = np.log(price/price.shift(1)).dropna()
 df['log_ret'] = log_ret
 
@@ -12,15 +12,16 @@ window = [10, 20, 60]
  
 for i in range(len(window)):
 
-      standard_deviation = df['log_ret'].rolling(i).std()
+      standard_deviation = log_ret.rolling(i).std()
       print(f'Standard deviation in {i}-days:{standard_deviation}') 
 
 
-def MA(period,prices):
+def MA(period,price):
+      return price.rolling(period).mean()
     
           
 #calculate 50-day MA
-print(MA(period=50,prices=df))
+print(MA(period=50,price=price))
 
 #calculate 200-day MA
-print(MA(period=200,prices=df))
+print(MA(period=200,price=price))
