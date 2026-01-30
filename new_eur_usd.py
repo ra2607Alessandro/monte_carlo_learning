75a1d6b0211d349a23098027c202da4955afdbd8
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-01-30 07:40:27 +0100

    man

--- new_eur_usd.py
+++ new_eur_usd.py
@@ -3,10 +3,13 @@ import pandas as pd
 df = pd.read_csv("2EUR_USD.csv").set_index('Date')
 df = df.sort_index()
 # look at the 15 min chart when setting the ranges high and low
-# London open 9.00 AM
+# London open 8.00 AM
 # Wall Street open 15.30 PM
 
 range_high = df['Range High']
 range_low = df['Range Low']
+m = 0.0015
 print(f'Range High: {range_high.iloc[-1]}')
 print(f'Range Low: {range_low.iloc[-1]}')
+print(f'Stop Loss if High: {range_high.iloc[-1]-m }')
+print(f'Stop Loss if Low: {range_high.iloc[-1] - m}')
