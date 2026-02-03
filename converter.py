d1b8292eda6ad50706f5ed7ce87b79038d853c83
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-03 07:05:24 +0100

    man

--- converter.py
+++ converter.py
@@ -1,8 +1,8 @@
 import csv 
 
 new_csv_data = []
-files= ['EURUSD_1_M_BID_19.01.2026-30.01.2026.csv','EURUSD_1_M_BID_01.12.2025-19.01.2026.csv','EURUSD_1_M_BID_19.01.2026-30.01.2026.csv',
-        'EURUSD_1_M_BID_01.09.2025-02.12.2025.csv','EURUSD_1_M_BID_05.05.2025-02.09.2025.csv','EURUSD_1_M_BID_01.01.2025-06.05.2025.csv']
+files= ['EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv','EURUSD_Candlestick_1_M_BID_01.12.2025-19.01.2026.csv','EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv',
+        'EURUSD_Candlestick_1_M_BID_01.09.2025-02.12.2025.csv','EURUSD_Candlestick_1_M_BID_05.05.2025-02.09.2025.csv','EURUSD_Candlestick_1_M_BID_01.01.2025-06.05.2025.csv']
 
 for file in files:
     with open(file) as csvfile:
@@ -22,3 +22,4 @@ with open('EURUSD_Candlesticks_1_M_BID_01.01.2025-30.01.2026.csv','w') as csvfil
         'Gmt time','Open','High',
         'Low','Close','Volume'
     ])
+    writer.writerows(new_csv_data)
