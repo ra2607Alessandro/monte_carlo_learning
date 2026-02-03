e2d5f151e69dc60f430a9bcb2a92c4753dfff16a
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-03 06:54:24 +0100

    converting

--- converter.py
+++ converter.py
@@ -3,3 +3,16 @@ import csv
 new_csv_data = []
 files= ['EURUSD_1_M_BID_19.01.2026-30.01.2026.csv','EURUSD_1_M_BID_01.12.2025-19.01.2026.csv','EURUSD_1_M_BID_19.01.2026-30.01.2026.csv',
         'EURUSD_1_M_BID_01.09.2025-02.12.2025.csv','EURUSD_1_M_BID_05.05.2025-02.09.2025.csv','EURUSD_1_M_BID_01.01.2025-06.05.2025.csv']
+
+for file in files:
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            new_csv_data.append([
+                row['Gmt time'],
+                row['Open'],
+                row['High'],
+                row['Low'],
+                row['Close'],
+                row['Volume']
+            ])
