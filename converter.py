74d22dd44693e7db1e1b3ca61fe291fc9ada7388
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-03 18:36:05 +0100

    cosa iutile

--- converter.py
+++ converter.py
@@ -1,11 +1,15 @@
 import csv 
 
 new_csv_data = []
-files= ['EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv','EURUSD_Candlestick_1_M_BID_01.12.2025-19.01.2026.csv','EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv',
-        'EURUSD_Candlestick_1_M_BID_01.09.2025-02.12.2025.csv','EURUSD_Candlestick_1_M_BID_05.05.2025-02.09.2025.csv','EURUSD_Candlestick_1_M_BID_01.01.2025-06.05.2025.csv']
+files= [    
+        'EURUSD_Candlestick_1_M_BID_01.01.2025-06.05.2025.csv',
+        'EURUSD_Candlestick_1_M_BID_05.05.2025-02.09.2025.csv',
+        'EURUSD_Candlestick_1_M_BID_01.09.2025-02.12.2025.csv',
+        'EURUSD_Candlestick_1_M_BID_01.12.2025-19.01.2026.csv',
+        'EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv']
 
-for file in files:
-    with open(file) as csvfile:
+for i in range(len(files)):
+    with open(files[i]) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             new_csv_data.append([
