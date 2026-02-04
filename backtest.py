69feffedc76fcb7f5449fba3b6fe0a97e7152b7f
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-04 15:16:56 +0100

    yes bro

--- backtest.py
+++ backtest.py
@@ -7,14 +7,24 @@ def get_asian_date(day,month,year):
   date_real = f'{day}.{month}.{year}' 
   morning_asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=0,minute=30,second=0,microsecond=0)}' &
                     df['Gmt time'] == f'{date_real} {datetime.time(hour=7,minute=30,second=0,microsecond=0)}')]
+  
+  afternoon_asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=9,minute=0,second=0,microsecond=0)}' &
+                    df['Gmt time'] == f'{date_real} {datetime.time(hour=15,minute=0,second=0,microsecond=0)}')]
 
-  range_high = morning_asian_session['High'].max()
-  range_low = morning_asian_session['Low'].min()
-  range_size = range_high - range_low
+  range_high_mornig = morning_asian_session['High'].max()
+  range_low_morning = morning_asian_session['Low'].min()
+  range_size_morning = range_high_mornig - range_low_morning
    
-  print('Range High: ',range_high)
-  print('Range Low: ',range_low)
-  print('Range Size: ',range_size)
+  range_high_afternoon = afternoon_asian_session["High"].max()
+  range_low_afternoon = afternoon_asian_session["Low"].min()
+  range_size_afternoon = range_high_afternoon - range_low_afternoon
+
+  print('Range High (Morning) : ',range_high_mornig)
+  print('Range Low (Morning) : ',range_low_morning)
+  print('Range Size (Morning) : ',range_size_morning)
+  print('Range High (Afternoon) : ',range_high_afternoon)
+  print('Range Low (Afternoon) : ',range_low_afternoon)
+  print('Range Size (Afternoon) : ',range_size_afternoon)
 
 
 get_asian_date()
