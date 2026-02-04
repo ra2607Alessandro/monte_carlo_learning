f0cd51e3ea5d9185e39620655bd0a3c68b569acd
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-04 08:12:25 +0100

    man

--- backtest.py
+++ backtest.py
@@ -11,3 +11,6 @@ date_real = f'{date.day}.{date.month}.{date.year}'
 asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=0,minute=30,second=0,microsecond=0)}' &
                     df['Gmt time'] == f'{date_real} {datetime.time(hour=7,minute=30,second=0,microsecond=0)}')]
 
+range_high = asian_session['High'].max()
+range_low = asian_session['Low'].min()
+range_size = range_high - range_low
