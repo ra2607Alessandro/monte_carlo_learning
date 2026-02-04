ff17a0fab70ebb79ea84982dcd08b2e72326c719
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-04 16:59:59 +0100

    or you

--- backtest.py
+++ backtest.py
@@ -7,14 +7,13 @@ df = df.dropna(subset=['Gmt time']).sort_values('Gmt time').reset_index(drop=Tru
 df['date'] = df['Gmt time'].dt.date
 df['hour'] = df['Gmt time'].dt.hour
 
-def get_asian_date(day,month,year):
-  date_real = f'{day}.{month}.{year}' 
-  for i in range (1,8): 
-   morning_asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=i,minute=0,second=0,microsecond=0)}') &
-                    (df['Gmt time'] == f'{date_real} {datetime.time(hour=8,minute=0,second=0,microsecond=0)}')]
+def get_asian_date(date):
+   
+  for i in range (1,8):
+   morning_asian_session = df[df['date'] == date & df['hour'] == datetime.time(hour=i)] 
   
-   afternoon_asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=8,minute=0,second=0,microsecond=0)}') &
-                    (df['Gmt time'] == f'{date_real} {datetime.time(hour=14,minute=0,second=0,microsecond=0)}')]
+  for i in range (8,14):
+   afternoon_asian_session = df[df['date'] == date & df['hour'] == datetime.time(hour=i)] 
 
   range_high_mornig = morning_asian_session['High'].max()
   range_low_morning = morning_asian_session['Low'].min()
@@ -32,7 +31,6 @@ def get_asian_date(day,month,year):
   print('Range Size (Afternoon) : ',range_size_afternoon)
 
 
-get_asian_date(30,'01',2026)
 
 def breakouts(range_high,range_low,session,date):
   if session.lower() == 'london':
@@ -41,18 +39,18 @@ def breakouts(range_high,range_low,session,date):
     start_hour,end_hour = 14, 18
 
   for i in range(start_hour,end_hour):
-     session_data = df[(df['Gmt time'] == f'{date} {datetime.time(hour=i,minute=0,second=0,microsecond=0)}') &
-                    (df['Gmt time'] == f'{date} {datetime.time(hour=end_hour,minute=0,second=0,microsecond=0)}')]
-  
-     if len(session_data) == 0:
+     session_data = df[df['date'] == date & df['hour'] == datetime.time(hour=i)] 
+     session_df = df[session_data].copy()
+
+     if len(session_df) == 0:
       return None
   
-     long_breakout = session_data[session_data['Close'] > range_high ]
+     long_breakout = session_df[session_data['Close'] > range_high ]
      if len(long_breakout) > 0:
         
         first_break = long_breakout.iloc[0]
         # Check if price stayed above for 15 minutes (15 candles)
-        break_hold = first_break['Gmt time']
+        break_hold = first_break['hour']
         for i in range(15):
           
         
