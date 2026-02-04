3f21afbad4fa5cb677e649aba91e2c8d03414d55
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-04 08:08:19 +0100

    inputs and asias session

---
+++ backtest.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import datetime 
+
+df = pd.read_csv('EURUSD_Candlesticks_1_M_BID_01.01.2025-30.01.2026.csv')
+
+day = int(input('Day (i.e 23/1):'))
+month = int(input('Month (i.e 7/1):'))
+year = int(input('Year (i.e 2026/2000):'))
+date = datetime.date(year=year,month=month,day=day)
+date_real = f'{date.day}.{date.month}.{date.year}' 
+asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=0,minute=30,second=0,microsecond=0)}' &
+                    df['Gmt time'] == f'{date_real} {datetime.time(hour=7,minute=30,second=0,microsecond=0)}')]
+
