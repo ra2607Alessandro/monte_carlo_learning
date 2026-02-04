91d1543b1d8754eaa22d550ca055a36324b9c0d7
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-04 08:22:35 +0100

    have the df and everything else. needs to insert breakout handling.

--- backtest.py
+++ backtest.py
@@ -3,14 +3,18 @@ import datetime
 
 df = pd.read_csv('EURUSD_Candlesticks_1_M_BID_01.01.2025-30.01.2026.csv')
 
-day = int(input('Day (i.e 23/1):'))
-month = int(input('Month (i.e 7/1):'))
-year = int(input('Year (i.e 2026/2000):'))
-date = datetime.date(year=year,month=month,day=day)
-date_real = f'{date.day}.{date.month}.{date.year}' 
-asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=0,minute=30,second=0,microsecond=0)}' &
+def get_asian_date(day,month,year)
+  date_real = f'{day}.{month}.{year}' 
+  morning_asian_session = df[(df['Gmt time'] == f'{date_real} {datetime.time(hour=0,minute=30,second=0,microsecond=0)}' &
                     df['Gmt time'] == f'{date_real} {datetime.time(hour=7,minute=30,second=0,microsecond=0)}')]
 
-range_high = asian_session['High'].max()
-range_low = asian_session['Low'].min()
-range_size = range_high - range_low
+  range_high = morning_asian_session['High'].max()
+  range_low = morning_asian_session['Low'].min()
+  range_size = range_high - range_low
+   
+  print('Range High: ',range_high)
+  print('Range Low: ',range_low)
+  print('Range Size: ',range_size)
+
+
+get_asian_date()
