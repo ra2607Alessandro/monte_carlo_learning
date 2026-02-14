c28359fab2d69e8b4abce39f116d5f0b22f4d324
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-14 09:36:02 +0100

    por

--- backtest.py
+++ backtest.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Load and preprocess data
 # parse datetimes (dayfirst True) and drop rows where parsing failed
-df = pd.read_csv('GBPUSD_M1.csv', delim_whitespace=True)
+df = pd.read_csv('GBPUSD_M1.csv',sep='\s+')
 if 'Time' not in df.columns:
    time_col = df.columns[0]
    df.rename(columns={time_col:'Time'},inplace=True)
