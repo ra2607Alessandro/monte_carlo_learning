489cdbae474eedb8de6a852f9c3f91c036237ea7
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-17 06:47:23 +0100

    man

--- converter.py
+++ converter.py
@@ -7,15 +7,15 @@ files= [
         'EURUSD_Candlestick_1_M_BID_01.09.2025-02.12.2025.csv',
         'EURUSD_Candlestick_1_M_BID_01.12.2025-19.01.2026.csv',
         'EURUSD_Candlestick_1_M_BID_19.01.2026-30.01.2026.csv',
-        'DAT_MT_EURUSD_M1_202602.csv']
-
+        'DAT_MT_EURUSD_M1_202602'
+        ]
 for i in range(len(files)):
     with open(files[i]) as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row[0] == 'Day' and row[1] == 'Hour':
-                gmt_time = []
-                gmt_time.append[row['Day'],row['Hour']] 
+        fieldname = reader.fieldnames
+        if 'Day' in fieldname and 'Hour' in fieldname and not 'Gmt time':
+         for row in reader:
+                gmt_time = f'{row['Day']} {row['Hour']}'
                 new_csv_data.append([
                 gmt_time, 
                 row['Open'],
@@ -23,7 +23,8 @@ for i in range(len(files)):
                 row['Low'],
                 row['Close'],
                 row['Volume']])
-            else:
+        else:
+            for row in reader:
                 new_csv_data.append([
                 row['Gmt time'],
                 row['Open'],
@@ -32,7 +33,10 @@ for i in range(len(files)):
                 row['Close'],
                 row['Volume']
             ])
-with open('EURUSD_Candlesticks_1_M_BID_01.01.2025-30.01.2026.csv','w') as csvfile:
+
+
+
+with open('EURUSD_Candlesticks_1_M_BID_01.01.2025-13.02.2026.csv','w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow([
         'Gmt time','Open','High',
