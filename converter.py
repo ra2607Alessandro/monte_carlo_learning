1164ec7b4446d2f0891db2aace87431ee5f8add9
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-22 11:36:56 +0100

    bro

--- converter.py
+++ converter.py
@@ -21,7 +21,9 @@ for i in range(len(files)):
 
         if 'Day' in fieldname and 'Hour' in fieldname:
             for row in reader:
-                gmt_time = f'{row["Day"]} {row["Hour"]}'
+                dt = datetime.strptime(row["Hour"],'%H:%M')
+                gmt_hour = dt.strftime('%d.%m.%Y %H:%M:%S.000')
+                gmt_time = f'{row['Day']} {gmt_hour}'
                 new_csv_data[gmt_time] = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
 
         elif 'Time' in fieldname:
@@ -36,7 +38,8 @@ for i in range(len(files)):
                 new_csv_data[gmt_time] = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
 
 # Sort by timestamp before writing
-sorted_data = sorted(new_csv_data.items())
+sorted_data = sorted(new_csv_data.items(), key=lambda x: datetime.strptime(x[0],format='%d.%m.%Y %H:%M:%S.000'))
+
 
 with open('EURUSD_Candlesticks_1_M_BID_01.01.2025-20.02.2026.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
