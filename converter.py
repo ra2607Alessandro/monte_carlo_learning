adc2a3a0f9e2223af8434baee55811e44f770339
ra2607Alessandro <2alessandro.aimar@gmail.com>
2026-02-24 07:01:55 +0100

    what's that

--- converter.py
+++ converter.py
@@ -17,7 +17,7 @@ for i in range(len(files)):
     with open(files[i]) as csvfile:
         reader = csv.DictReader(csvfile)
         fieldname = reader.fieldnames
-        print(files[i], fieldname)
+        print(files[i],fieldname)
 
         if 'Day' in fieldname and 'Hour' in fieldname:
             for row in reader:
@@ -25,11 +25,13 @@ for i in range(len(files)):
                 gmt_time = dt.strftime('%d.%m.%Y %H:%M:%S.000')
                 new_csv_data[gmt_time] = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
 
-        if 'Time' in fieldname:
-            for row in reader:
-                dt = datetime.strptime(row['Time'], '%Y-%m-%d %H:%M:%S')
-                gmt_time = dt.strftime('%d.%m.%Y %H:%M:%S.000')
-                new_csv_data[gmt_time] = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
+        if 'Time' in fieldname or 'Time\tOpen\tHigh\tLow\tClose\tVolume' in fieldname:
+            with open(files[i]) as file:
+                read = csv.DictReader(file,delimiter='\t')
+                for row in read:
+                   dt = datetime.strptime(row['Time'], '%Y-%m-%d %H:%M:%S')
+                   gmt_time = dt.strftime('%d.%m.%Y %H:%M:%S.000')
+                   new_csv_data[gmt_time] = [row['Open'], row['High'], row['Low'], row['Close'], row['Volume']]
 
         if 'Gmt time' in fieldname:
             for row in reader:
@@ -39,10 +41,11 @@ for i in range(len(files)):
 # Sort by timestamp before writing
 sorted_data = sorted(new_csv_data.items(), key=lambda x: datetime.strptime(x[0], '%d.%m.%Y %H:%M:%S.000'))
 
-print(max(new_csv_data.keys()))
+
 
 with open('EURUSD_Candlesticks_1_M_BID_01.01.2025-20.02.2026.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['Gmt time', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    print(max(new_csv_data.keys()))
     for gmt_time, values in sorted_data:
         writer.writerow([gmt_time] + values)
